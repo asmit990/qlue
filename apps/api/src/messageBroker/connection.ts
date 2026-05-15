711b92307fa0ac0bf87e70e0f6cfd8b140404efb
asmit990 <asmitpandeyw4354@gmail.com>
2026-05-16 00:40:37 +0530

    rabbitMQ something in that

--- apps/api/src/messageBroker/connection.ts
+++ apps/api/src/messageBroker/connection.ts
@@ -3,11 +3,17 @@ import amqp from 'amqplib';
 let channel: amqp.Channel;
 
 export const connectRabbitMQ = async () => {
-    const connection = await amqp.connect('amqp://localhost');
+    const connection = await amqp.connect(
+        process.env.RABBITMQ_URL!
+    );
+
     channel = await connection.createChannel();
-    await channel.assertQueue('query_queue', { durable: true });
+
+    await channel.assertQueue('query_queue', {
+        durable: true,
+    });
+
     console.log('Connected to RabbitMQ');
-    return channel;
 };
 
 export const getChannel = () => channel;
