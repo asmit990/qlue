0493bfc93d727059b65d3db5e471023583bf90bc
asmit990 <asmitpandeyw4354@gmail.com>
2026-05-16 02:38:07 +0530

    fix: tsconfig build errors resolved

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+// @ts-ignore: allow side-effect CSS import
 import "./index.css";
 import App from "./App.tsx";
 
