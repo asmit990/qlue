6a2e13fb905224c3afd2a0af100090f226280514
asmit990 <asmitpandeyw4354@gmail.com>
2026-07-21 11:54:47 +0530

    feat: android and ios compatible app via capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'qlue.cloud',
+  appName: 'qlue',
+  webDir: 'apps/web/dist'
+};
+
+export default config;
